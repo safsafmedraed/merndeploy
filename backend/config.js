3ce12d6227fa652b724f8088d5a16f03a42b233e
safsaf mohamed raed <mohamedraed.safsaf@esprit.tn>
2020-04-22 20:10:20 +0100

    config a commun email for all my partners

--- backend/config.js
+++ backend/config.js
@@ -1,7 +1,7 @@
 module.exports = {
     JWT_SECRET: 'codeworkrauthentication',
-    EMAIL_ADDRESS: 'mohamedraed.safsaf@esprit.tn',
-    EMAIL_PASSWORD: '****',
+    EMAIL_ADDRESS: 'thewitchertwin3@gmail.com',
+    EMAIL_PASSWORD: 'azerty#123',
     'googleAuth': {
         'clientID': '637588511792-tq8rv8u0c2qif45bpkuv0b0vfvdk7knp.apps.googleusercontent.com',
         'clientSecret': '3PWNUffpG6j9pg7WlZPcWnqp',
