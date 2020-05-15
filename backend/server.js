7f44b552428d40d5571ff7dad8666f364680cba8
safsafmedraed <mohamedraed.safsaf@esprit.tn>
2020-05-16 00:35:06 +0100

    deploy 0.1

--- backend/server.js
+++ backend/server.js
@@ -94,7 +94,7 @@ app.use('/presence', presence);
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('../build'))
   app.get('*', (req, res) => {
-    res.sendFile(__dirname, '../', 'build', 'index.html')
+    res.sendFile(path.resolve(__dirname, '../', 'build', 'index.html'))
   })
 }
 app.listen(port, () => {
